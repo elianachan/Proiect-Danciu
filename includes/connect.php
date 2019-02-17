5dda0a658aa2da5544d5e981dd4c5f6c83fab8f0
elianachan <elianamanescu@gmail.com>
2019-02-17 12:37:43 +0200

    Offers added

--- includes/connect.php
+++ includes/connect.php
@@ -6,7 +6,7 @@ if(!$connection){
     die('Database connection failed' . mysqli_error($connection));
 }
 
-$select_db = mysqli_select_db($connection, 'jobpaltform');
+$select_db = mysqli_select_db($connection, 'jobplatform');
 
 if(!$select_db){
     die('Database selection failed' . mysqli_error($connection));
